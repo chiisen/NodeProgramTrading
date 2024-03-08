452f72b5d6295e971e772e803ac6e4f6b44e02e9
sam <samliao1@auforce.com>
2024-03-08 13:10:23 +0800

    feat: 移除兩個 twse-scraper.service.ts 其中之一的檔案

--- scraper/src/app.module.ts
+++ scraper/src/app.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { ScraperModule } from './scraper/scraper.module';
 // 透過 `nest g service twse-scraper --flat --no-spec` 建立的 twse-scraper.service 服務，與 `scraper` 目錄的 twse-scraper.service 服務才能正確執行
-import { TwseScraperService } from './twse-scraper.service';
+import { TwseScraperService } from './scraper/twse-scraper.service';
+import { HttpModule } from '@nestjs/axios';
 
 @Module({
-  imports: [ScraperModule],
+  imports: [HttpModule, ScraperModule],
   providers: [TwseScraperService],
 })
 export class AppModule {}
